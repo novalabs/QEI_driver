3179cb30624d45d80b97fff7fa55461736d1d5d8
Davide Rizzi <d.rizzi@novalabs.io>
2016-06-16 16:03:01 +0200

    First commit

---
+++ include/QEI.hpp
@@ -0,0 +1,78 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
+#pragma once
+
+#include <Configuration.hpp>
+
+#include <Core/HW/QEI.hpp>
+#include <Core/MW/CoreSensor.hpp>
+#include <QEI_driver/QEI_DeltaConfiguration.hpp>
+
+namespace sensors {
+   class QEI
+   {
+public:
+      QEI(
+         Core::HW::QEI& qei
+      );
+
+      virtual
+      ~QEI();
+
+public:
+      bool
+      probe();
+
+
+public:
+      Core::HW::QEI& _qei;
+   };
+
+   class QEI_Delta:
+      public Core::MW::CoreSensor<Configuration::QEI_DELTA_DATATYPE>
+   {
+public:
+      QEI_Delta(
+         QEI& device
+      );
+
+      virtual
+      ~QEI_Delta();
+
+public:
+      QEI_DeltaConfiguration configuration;
+
+private:
+public:
+      bool
+      init();
+
+      bool
+      start();
+
+      bool
+      stop();
+
+      bool
+      waitUntilReady();
+
+      bool
+      update();
+
+      void
+      get(
+         DataType& data
+      );
+
+
+protected:
+      Core::MW::Time _timestamp;
+
+private:
+      QEI& _device;
+   };
+}
---
+++ src/QEI.cpp
@@ -0,0 +1,89 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
+#include <QEI_driver/QEI.hpp>
+#include <Module.hpp>
+
+#include <Core/Utils/Math/Constants.hpp>
+
+namespace sensors {
+   QEI::QEI(
+      Core::HW::QEI& qei
+   ) : _qei(qei) {}
+
+   QEI::~QEI()
+   {}
+
+   bool
+   QEI::probe()
+   {
+      return true;
+   } // QEI::init
+
+   QEI_Delta::QEI_Delta(
+      QEI& device
+   ) : _timestamp(Core::MW::Time::IMMEDIATE), _device(device)
+   {
+      configuration.period = 20.0f;
+      configuration.ticks  = 0.0f;
+   }
+
+   QEI_Delta::~QEI_Delta()
+   {}
+
+   bool
+   QEI_Delta::init()
+   {
+      CORE_ASSERT(configuration.ticks != 0);
+      return true;
+   } // QEI::init
+
+   bool
+   QEI_Delta::start()
+   {
+      _device._qei.enable();
+      _timestamp = Core::MW::Time::IMMEDIATE;
+      return true;
+   }
+
+   bool
+   QEI_Delta::stop()
+   {
+      _device._qei.disable();
+      return true;
+   }
+
+   bool
+   QEI_Delta::update()
+   {
+      return true;
+   } // QEI_Delta::update
+
+   void
+   QEI_Delta::get(
+      DataType& data
+   )
+   {
+      data.value = _device._qei.getDelta() / (float)configuration.ticks * (2.0f * Core::Utils::Math::Constants::pi<float>());
+   } // QEI::update
+
+   bool
+   QEI_Delta::waitUntilReady()
+   {
+      // FIXME !!!!!
+      /*
+         if (_timestamp != Core::MW::Time::IMMEDIATE) {
+         Core::MW::Thread::sleep_until(_timestamp + Core::MW::Time::ms(configuration.period));
+         }
+
+         _timestamp = Core::MW::Time::now();
+       */
+
+      chThdSleepMilliseconds((uint16_t)configuration.period);
+
+      return true;
+   }
+}
